dbadd14d3ac8b91e391c73a087509518f886c002
Vlad <botafogos@yandex.ru>
2024-02-19 00:22:03 +0300

    MVVM components

---
+++ ClientsApp/Model/Client.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ClientsApp.Model
+{
+    public sealed class Client: INotifyPropertyChanged
+    {
+        private long _id;
+        private string? _name;
+        private string? _status;
+
+        public long Id 
+        { 
+            get { return _id; } 
+            set
+            {
+                _id = value;
+                OnPropertyChanged(nameof(Id));
+            }
+        }
+        public string? Name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+
+        public string? Status
+        {
+            get { return _status; }
+            set
+            {
+                _status = value;
+                OnPropertyChanged(nameof(Status));
+            }
+        }
+
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
---
+++ ClientsApp/ViewModel/AppViewModel.cs
@@ -0,0 +1,42 @@
+﻿using ClientsApp.Model;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ClientsApp.ViewModel
+{
+    public sealed class AppViewModel: INotifyPropertyChanged
+    {
+        private Client _selectedClients;
+        public Client SelectedClients 
+        { 
+            get { return _selectedClients; } 
+            set 
+            { 
+                _selectedClients = value; 
+                OnPropertyChanged(nameof(SelectedClients));
+            } 
+        }
+
+        public ObservableCollection<Client> Clients { get; set; }
+
+        public AppViewModel()
+        {
+            // test
+            Clients = new ObservableCollection<Client>()
+            {
+                new Client() { Id = 1, Name = "Vlad", Status = "Active"},
+                new Client() {Id = 2, Name = "Vova", Status = "Active"},
+                new Client() {Id = 3, Name = "Voldemar", Status = "Terminated"},
+                new Client() {Id = 4, Name = "Victor", Status = "Active"},
+            };
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
